b00f97939c92e4dd9a5787b68548002416085421
happy <happyrahman921@gmail.com>
2021-10-11 22:36:21 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return ;
+// }
+// const result = add(13, 43);
+// console.log(result);
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);  
+
+// const premik = {name: 'smart guy', phone: 1234};
+// console.log(premik.gf);
+
+let fun = undefined;
+// console.log(fun);
+
+ let ages = [2,3,4];
+ console.log(ages[11]);
+
