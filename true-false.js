d0534c476a4acce205f7d01f981912860e45005b
happy <happyrahman921@gmail.com>
2021-10-11 22:23:20 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//0 F
+//"" F
+//undefined F
+//null F
+//NaN F
+// '0' T
+//[] T
+//{} T
+
+
+// const age =0;
+let name = false ;
+
+// const name = 's'
+if(name){
+    console.log("condition is true");
+
+}
+else{
+    console.log('condtion is false');
+}
