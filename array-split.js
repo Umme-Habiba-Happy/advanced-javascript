27db22ba885a6b066701fc4f2305af6c8341d3ea
happy <happyrahman921@gmail.com>
2021-10-13 16:38:14 +0600

    learning scope

---
+++ array-split.js
@@ -0,0 +1,28 @@
+// const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2, 5) //index
+
+// const removed = nums.splice(2,3,55);
+// console.log(removed);
+// console.log(part);  
+// console.log(nums);
+
+// const together = nums.join(", ");
+// console.log(together);
+
+
+
+
+
+const freinds = ['alia', 'beli', 'mili', 'tana' , 'kokil', 'sara']
+
+// const removed = freinds.splice(1,4, 'happy');
+// console.log(removed);
+// console.log(freinds);
+
+// const part = freinds.slice(2,4);
+// console.log(part);
+// console.log(freinds);
+
+const together = freinds.join(' hailey '); // join method 
+console.log(together)
+
---
+++ scope.js
@@ -0,0 +1,18 @@
+function sum(first, second) {
+    let result = first + second;
+    // console.log(result);
+    return result;
+ 
+} 
+const result = sum(3, 7);
+
+// console.log(result);
+
+if(result >9){
+    var mood = 'happy';
+     mood = 'sad';
+     mood = 'cranky';
+     mood = 'full';
+    console.log(mood);
+}
+console.log(mood);
